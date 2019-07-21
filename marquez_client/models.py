106ae0495d2459f8a697089b39c9f25b134054dd
= <sairanjith.thalanki@wework.com>
2019-07-20 17:06:00 -0700

    Apply style

--- marquez_client/models.py
+++ marquez_client/models.py
@@ -20,142 +20,149 @@ class RunState(Enum):
     FAILED = 'FAILED'
     ABORTED = 'ABORTED'
 
+
 class Namespace:
-	def __init__(self, name, created_at, owner_name, description):
-		self._name = name
-		self._created_at = created_at
-		self._owner_name = owner_name
-		self._description = description
-	
-	@property
-	def name(self):
-		return self._name
+    def __init__(self, name, created_at, owner_name, description):
+        self._name = name
+        self._created_at = created_at
+        self._owner_name = owner_name
+        self._description = description
 
-	@property
-	def created_at(self):
-		return self._created_at
+    @property
+    def name(self):
+        return self._name
 
-	@property
-	def owner_name(self):
-		return self._owner_name
+    @property
+    def created_at(self):
+        return self._created_at
 
-	@property
-	def description(self):
-		return self._description
+    @property
+    def owner_name(self):
+        return self._owner_name
+
+    @property
+    def description(self):
+        return self._description
+
 
 class Job:
-	def __init__(self, name, created_at, updated_at, input_dataset_urns, output_dataset_urns, location, description):
-		self._name = name
-		self._created_at = created_at
-		self._updated_at = updated_at
-		self._input_dataset_urns = input_dataset_urns
-		self._output_dataset_urns = output_dataset_urns
-		self._location = location
-		self._description = description
+    def __init__(self, name, created_at, updated_at, input_dataset_urns,
+                 output_dataset_urns, location, description):
+        self._name = name
+        self._created_at = created_at
+        self._updated_at = updated_at
+        self._input_dataset_urns = input_dataset_urns
+        self._output_dataset_urns = output_dataset_urns
+        self._location = location
+        self._description = description
 
-	@property
-	def name(self):
-		return self._name
-	
-	@property
-	def created_at(self):
-		return self._created_at
+    @property
+    def name(self):
+        return self._name
 
-	@property
-	def updated_at(self):
-		return self._updated_at
-	
-	@property
-	def input_dataset_urns(self):
-		return self._input_dataset_urns
+    @property
+    def created_at(self):
+        return self._created_at
 
-	@property
-	def output_dataset_urns(self):
-		return self._output_dataset_urns
+    @property
+    def updated_at(self):
+        return self._updated_at
 
-	@property
-	def location(self):
-		return self._location
+    @property
+    def input_dataset_urns(self):
+        return self._input_dataset_urns
 
-	@property
-	def description(self):
-		return self._description
+    @property
+    def output_dataset_urns(self):
+        return self._output_dataset_urns
+
+    @property
+    def location(self):
+        return self._location
+
+    @property
+    def description(self):
+        return self._description
+
 
 class JobRun:
-	def __init__(self, run_id, nominal_start_time, nominal_end_time, run_args, run_state):
-		self._run_id = run_id
-		self._nominal_start_time = nominal_start_time
-		self._nominal_end_time = nominal_end_time
-		self._run_args = run_args
-		self._run_state =run_state
+    def __init__(self, run_id, nominal_start_time,
+                 nominal_end_time, run_args, run_state):
+        self._run_id = run_id
+        self._nominal_start_time = nominal_start_time
+        self._nominal_end_time = nominal_end_time
+        self._run_args = run_args
+        self._run_state = run_state
 
-	@property
-	def run_id(self):
-		return self._run_id
-	
-	@property
-	def nominal_start_time(self):
-		return self._nominal_start_time
-	
-	@property
-	def nominal_end_time(self):
-		return self._nominal_end_time
-	
-	@property
-	def run_args(self):
-		return self._run_args
-	
-	@property
-	def run_state(self):
-		return self._run_state
-	
+    @property
+    def run_id(self):
+        return self._run_id
+
+    @property
+    def nominal_start_time(self):
+        return self._nominal_start_time
+
+    @property
+    def nominal_end_time(self):
+        return self._nominal_end_time
+
+    @property
+    def run_args(self):
+        return self._run_args
+
+    @property
+    def run_state(self):
+        return self._run_state
+
+
 class Datasource:
-	def __init__(self, name, created_at, urn, connection_url):
-		self._name = name
-		self._created_at = created_at
-		self._urn = urn
-		self._connection_url = connection_url
+    def __init__(self, name, created_at, urn, connection_url):
+        self._name = name
+        self._created_at = created_at
+        self._urn = urn
+        self._connection_url = connection_url
 
-	@property
-	def name(self):
-		return self._name
-	
-	@property
-	def created_at(self):
-		return self._created_at
-	
-	@property
-	def urn(self):
-		return self._urn
-	
-	@property
-	def connection_url(self):
-		return self._connection_url
-	
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def created_at(self):
+        return self._created_at
+
+    @property
+    def urn(self):
+        return self._urn
+
+    @property
+    def connection_url(self):
+        return self._connection_url
+
+
 class Dataset:
-	def __init__(self, name, created_at, urn, datasource_urn, description):
-		self._name = name
-		self._created_at = created_at
-		self._urn = urn
-		self._datasource_urn = datasource_urn
-		self._description = description
+    def __init__(self, name, created_at, urn, datasource_urn, description):
+        self._name = name
+        self._created_at = created_at
+        self._urn = urn
+        self._datasource_urn = datasource_urn
+        self._description = description
 
-	@property
-	def name(self):
-		return self._name
-	
-	@property
-	def created_at(self):
-		return self._created_at
-	
-	@property
-	def urn(self):
-		return self._urn
-	
-	@property
-	def datasource_urn(self):
-		return self._datasource_urn
+    @property
+    def name(self):
+        return self._name
 
-	@property
-	def description(self):
-		return self._description
+    @property
+    def created_at(self):
+        return self._created_at
+
+    @property
+    def urn(self):
+        return self._urn
+
+    @property
+    def datasource_urn(self):
+        return self._datasource_urn
+
+    @property
+    def description(self):
+        return self._description
